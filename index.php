cbefe6f0b31896490ee61cc5816501d2b0a1abd5
Carleosmar <karleosmar@hotmail.com>
2023-05-07 22:45:41 -0300

    estudo PHP 07.05.23

--- index.php
+++ index.php
@@ -18,6 +18,11 @@
                     <h3>8. Paradigma O.O</h3>
                         <ul>
                             <li>
+                                <a href="exercicio.php?dir=classes_objetos&file=construtor_destrutor">
+                                    Construtor e Destrutor
+                                </a>
+                            </li>
+                            <li>
                                 <a href="exercicio.php?dir=classes_objetos&file=classe_data">
                                     Desafio Data
                                 </a>
