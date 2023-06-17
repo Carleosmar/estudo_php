66a6315903300801ba0ff230e72bf29bc82fd856
Carleosmar De Jesus <carleosmar.jesus@defesacivil.pms.ad>
2023-06-17 15:00:41 -0300

    estudo php 17.06.23

--- classes_objetos/classe_data.php
+++ classes_objetos/classe_data.php
@@ -2,7 +2,7 @@
 
 <?php
 
-class data {
+class Data {
     public $dia = 1;
     public $mes = 1;
     public $ano = 1970;
@@ -10,11 +10,10 @@ class data {
     public function apresentar() {
         return "{$this->dia}/{$this->mes}/{$this->ano}";
     }
-
 }
 
-$aniversario = new data();
+$aniversario = new Data();
 $aniversario->dia = 11;
-$aniversario->mes = 05;
+$aniversario->mes = 5;
 $aniversario->ano = 1996;
-echo $aniversario->apresentar(), '<br>';
+echo $aniversario->apresentar();
