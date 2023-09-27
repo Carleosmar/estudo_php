485e079a72fe6bbea53ad66ea3169e8a6bb6999d
carleosmar <karleoscuri@gmail.com>
2023-09-26 21:39:43 -0300

    Inserindo registro no banco

--- menu.php
+++ menu.php
@@ -3,6 +3,11 @@
         <h3>15. BD</h3>
             <ul>
                 <li>
+                    <a href="exercicio.php?dir=db&file=inserir_1">
+                        Inserir Registro #01
+                    </a>
+                </li>
+                <li>
                     <a href="exercicio.php?dir=db&file=criar_tabela">
                         Criar Tabela
                     </a>
